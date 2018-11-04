4b7b00ffe2c3069620ca873c1c678b1ccabb0c81
Jason Pettrone <jasonpettrone@gmail.com>
2018-11-04 01:01:34 -0400

    Implemented ability to easily draw a DotGraph

--- src/DrawGraph.java
+++ src/DrawGraph.java
@@ -1,6 +1,39 @@
-public class DrawGraph {
+import processing.core.PApplet;
 
-    public DrawGraph(DotGraph g){
+public class DrawGraph extends PApplet{
 
+    private int numXDots;
+    private int numYDots;
+    private float dotSize;
+    private float xDotSeperator;
+    private float yDotSeperator;
+    private DotGraph g;
+
+    public DrawGraph(DotGraph g, int windowXSize, int windowYSize){
+
+        this.g = g;
+        numXDots = g.graph.length;
+        numYDots = g.graph[0].length;
+        this.xDotSeperator = (float)windowXSize / numXDots;
+        this.yDotSeperator = (float)windowYSize / numYDots;
+        windowXSize -= xDotSeperator;
+        windowYSize -= yDotSeperator;
+        this.xDotSeperator = (float)windowXSize / numXDots;
+        this.yDotSeperator = (float)windowYSize / numYDots;
+        this.dotSize = 30;//(float)(windowXSize * windowYSize) / (numXDots * numYDots);
+    }
+
+    public void draw(Main m){
+
+        float xPos = xDotSeperator;
+        for(int i = 0; i < numXDots; ++i){
+            float yPos = yDotSeperator;
+            m.ellipse(xPos,yPos,dotSize,dotSize);
+            for(int j = 0; j < numYDots; ++j){
+                m.ellipse(xPos, yPos,dotSize,dotSize);
+                yPos += yDotSeperator;
+            }
+            xPos += xDotSeperator;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,16 @@ import processing.core.PApplet;
 
 public class Main extends PApplet {
 
-    int xOffset = 100;
-    int yOffset = 50;
-    int numXDots = 6;
-    int numYDots = 6;
-    int dotSpacing = 100;
-    int dotSize = 40;
+    DotGraph g = new DotGraph(6,6);
+    DrawGraph drawGraph = new DrawGraph(g,600,600);
 
     public void settings(){
-        DotGraph g = new DotGraph(6,6);
         size(600, 600);
     }
 
-    public void draw(){
+    public void draw() {
         background(0);
-        // Draw the dots to the window
-        int xSpacing = 0;
-        int ySpacing = 0;
-        for(int i = 0; i < numXDots - 1; ++i){
-            ySpacing = 0;
-            ellipse(xSpacing+xOffset,ySpacing + yOffset,dotSize,dotSize);
-            for(int j = 0; j < numYDots - 1; ++j){
-                ySpacing += dotSpacing;
-                ellipse(xSpacing+xOffset, ySpacing + yOffset, dotSize,dotSize);
-            }
-            xSpacing += dotSpacing;
-        }
-        ellipse(mouseX, mouseY, 20, 20);
+        drawGraph.draw(this);
     }
 
     public static void main(String[] args){
